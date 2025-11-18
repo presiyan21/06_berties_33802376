6ddf91adc542ad389ec2486f9db0afec926189e9
presiyan21 <149686177+presiyan21@users.noreply.github.com>
2025-11-18 21:00:16 +0000

    lab07

--- insert_test_data.sql
+++ insert_test_data.sql
@@ -3,6 +3,6 @@
 USE berties_books;
 
 INSERT INTO users (username, first_name, last_name, email, hashed_password)
-VALUES ('gold', 'Gillian', 'Oldman', 'gold@bertiesbooks.com', '$2b$10$OgykfOmqHZXaOUmJgc8IF.GMJQw2C8BWuMcqwlNMcP/VnfnntA6hq');
+VALUES ('gold', 'Gillian', 'Oldman', 'gold@bertiesbooks.com', '$2b$10$VBXYGfsxWAqfGo3ex8P7SexNgAxvsh1HjxYJZV9vy5nXTS70L3Lp');
 
 INSERT INTO books (name, price)VALUES('Brighton Rock', 20.25),('Brave New World', 25.00), ('Animal Farm', 12.99) ;
